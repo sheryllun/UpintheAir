69e24b4a3b90d870ffbdec966e76e35ea44a8114
sheryllun <sheryl.v.lun@gmail.com>
2014-11-13 16:40:21 -0500

    Add initial files

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var airportData;
+
+$.getJSON('https://dl.dropboxusercontent.com/u/4292615/airports.json', function(data) {
+    airportData = data;
+  });
+
+$('#flightform').submit(function() {
+  event.preventDefault();
+  queryTravel();
+});
+
+function queryTravel() {
+  var departAirport = $('#depart').val().toUpperCase();
+  var arriveAirport = $('#arrive').val().toUpperCase();
+  for(var i = 0; i < airportData.length; i++) {
+    if(departAirport === airportData[i].iata) {
+      var departCity = airportData[i].city;
+    }
+    if(arriveAirport === airportData[i].iata) {
+      var arriveCity = airportData[i].city;
+    }
+  }
+  if(!departCity || !arriveCity) {
+    return alert('Please enter a valid airport code.');
+  }
+  var mapsURL = 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyCVzYqt4JIDK3C_xMQcNssaEKeDUxf45aA&origin='+ departAirport+' '+ departCity +'&destination='+ arriveAirport+' '+arriveCity +'&mode=flying';
+  $('iframe').attr('src', mapsURL);
+  $('#map').ScrollTo();
+}
+
+
+
