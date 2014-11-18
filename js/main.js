5b87e553c674411d3314c6edd8e1c1eaeef7970c
sheryllun <sheryl.v.lun@gmail.com>
2014-11-18 15:34:24 -0500

    Script for distance calculation and start coding third section of page

--- js/main.js
+++ js/main.js
@@ -16,9 +16,13 @@ function queryTravel() {
   for(var i = 0; i < airportData.length; i++) {
     if(departAirport === airportData[i].iata) {
       var departCity = airportData[i].city;
+      lat1 = airportData[i].latitude;
+      lon1 = airportData[i].longitude;
     }
     if(arriveAirport === airportData[i].iata) {
       var arriveCity = airportData[i].city;
+      lat2 = airportData[i].latitude;
+      lon2 = airportData[i].longitude;
     }
   }
   if(!departCity || !arriveCity) {
@@ -31,4 +35,26 @@ function queryTravel() {
 
 $('.uparrow').click(function() {
   $('.pageTop').ScrollTo();
+  calcDistance();
 });
+
+// Spherical law of cosines calculation script from http://www.movable-type.co.uk/scripts/latlong.html
+
+var lat1,lat2,lon1,lon2;
+
+function toRadians(coord) {
+  return coord * Math.PI/180;
+}
+
+function calcDistance() {
+  var φ1 = toRadians(lat1), 
+      φ2 = toRadians(lat2), 
+      Δλ = toRadians(lon2-lon1), 
+      R = 3959; // mi
+
+var distanceTraveled = Math.round(Math.acos( Math.sin(φ1)*Math.sin(φ2) + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * R);
+  
+ console.log(distanceTraveled);
+}
+
+
